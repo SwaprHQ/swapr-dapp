bad56b0b87c6f100eece1eb072ac58cd8024c489
AugustoL <me@augustol.com>
2020-10-29 17:18:34 -0300

    refactor(constants): Update pinned token bases used and pinned pairs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -32,29 +32,30 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DXD[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR]
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DXD[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR],
+  [ChainId.RINKEBY]: [...WETH_ONLY[ChainId.RINKEBY], DXD[ChainId.RINKEBY]]
 }
 
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT]
+  [ChainId.MAINNET]: [DXD[ChainId.MAINNET]],
+  [ChainId.RINKEBY]: [DXD[ChainId.RINKEBY]]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT]
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DXD[ChainId.MAINNET]],
+  [ChainId.RINKEBY]: [...WETH_ONLY[ChainId.RINKEBY], DXD[ChainId.RINKEBY]]
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
   [ChainId.MAINNET]: [
-    [
-      new Token(ChainId.MAINNET, '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643', 8, 'cDAI', 'Compound Dai'),
-      new Token(ChainId.MAINNET, '0x39AA39c021dfbaE8faC545936693aC917d5E7563', 8, 'cUSDC', 'Compound USD Coin')
-    ],
-    [USDC, USDT],
-    [DAI, USDT]
+    [WETH[ChainId.MAINNET],DXD[ChainId.MAINNET]],
+  ],
+  [ChainId.RINKEBY]: [
+    [WETH[ChainId.RINKEBY],DXD[ChainId.RINKEBY]],
   ]
 }
 
