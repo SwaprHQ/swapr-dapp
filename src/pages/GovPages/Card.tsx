78bf5e4cd28c02606316a95bc6121824b9707e3d
plum <xtonyu.ilu@gmail.com>
2021-02-04 03:39:49 -0500

    Revert "card per line"
    This reverts commit 382d862562e9c21af6a7c650a3f1522227d9c848.

--- src/pages/GovPages/Card.tsx
+++ src/pages/GovPages/Card.tsx
@@ -9,12 +9,12 @@ import { AutoRow } from '../../components/Row'
 import CurrencyLogo from '../../components/CurrencyLogo'
 import DoubleCurrencyLogo from '../../components/DoubleLogo'
 
-const LightCardWrap = styled(Card)<{ width: string }>`
+const LightCardWrap = styled(Card)`
   background: linear-gradient(113.18deg, rgba(255, 255, 255, 0.35) -0.1%, rgba(0, 0, 0, 0) 98.9%),
     ${({ theme }) => theme.dark1};
   background-blend-mode: overlay, normal;
   padding: 0.8rem;
-  width: ${({ width }) => width};
+  width: calc(25% - 6px);
   padding: 24px 30px;
   display: flex;
   flex-wrap: wrap;
@@ -81,7 +81,7 @@ export const GovCard = ({ currency, currency1, apy, proposals }: CardProps) => {
   if (currency1 === undefined) {
     // main governance page
     return (
-      <LightCardWrap onClick={onClick} width="calc(25% - 6px)">
+      <LightCardWrap onClick={onClick}>
         <AutoRow align="flex-end" justify="center">
           <CurrencyLogo size="20px" currency={currency} />
           <Text width="auto" marginTop="0" marginLeft="6px" fontWeight={600} fontSize="16px" lineHeight="20px">
@@ -100,7 +100,7 @@ export const GovCard = ({ currency, currency1, apy, proposals }: CardProps) => {
   } else {
     // pair page
     return (
-      <LightCardWrap width="calc(33% - 4px)">
+      <LightCardWrap>
         <AutoRow align="flex-end" justify="center">
           <LogoContainer size={doubleCurrencyLogoSize}>
             <DoubleCurrencyLogo size={doubleCurrencyLogoSize} currency0={currency1} currency1={currency} />
