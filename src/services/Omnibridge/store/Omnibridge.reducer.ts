6056916a41cb552d001377bdd0822acd29de911d
mix1o <smgpwwe@wp.pl>
2022-02-04 15:39:13 +0100

    Add UI reducer

--- src/services/Omnibridge/store/Omnibridge.reducer.ts
+++ src/services/Omnibridge/store/Omnibridge.reducer.ts
@@ -1,7 +1,59 @@
+import { ChainId } from '@swapr/sdk'
 import { combineReducers, createSlice } from '@reduxjs/toolkit'
 import { arbitrumReducers } from '../Arbitrum/ArbitrumBridge.reducer'
 
+type OmnibridgeInput = {
+  value: number
+  chainId: ChainId
+  address: string
+}
+
+type UIInitialState = Record<'from' | 'to', OmnibridgeInput> & {
+  statusButton: {
+    isError: boolean
+    isLoading: boolean
+    label: string
+  }
+  modal: {
+    show: boolean
+    status: string
+  }
+  filter: []
+}
+
+const initialState: UIInitialState = {
+  from: {
+    value: 0,
+    chainId: 1,
+    address: ''
+  },
+  to: {
+    value: 0,
+    chainId: 1,
+    address: ''
+  },
+  statusButton: {
+    isError: false,
+    isLoading: false,
+    label: ''
+  },
+  modal: {
+    show: false,
+    status: ''
+  },
+  filter: []
+}
+
+export const omnibridgeUISlice = createSlice({
+  name: 'UI',
+  initialState,
+  reducers: {}
+})
+
+export const { actions: omnibridgeUIActions } = omnibridgeUISlice
+
 const omnibridgeReducer = combineReducers({
+  UI: omnibridgeUISlice.reducer,
   ...arbitrumReducers
 })
 
