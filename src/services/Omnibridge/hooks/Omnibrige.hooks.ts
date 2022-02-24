cb586db9d3ec252ba7685a0f662ffb91cef470b3
Daniel Cybulka <danielcybulka@gmail.com>
2022-02-24 10:41:16 +0100

    draft UI implementation

--- src/services/Omnibridge/hooks/Omnibrige.hooks.ts
+++ src/services/Omnibridge/hooks/Omnibrige.hooks.ts
@@ -1,7 +1,9 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { AppState } from '../../../state'
 import { useActiveWeb3React } from '../../../hooks'
 import { selectAllTokensPerChain, selectAllActiveTokens, selectAllLists } from '../store/Omnibridge.selectors'
+import { commonActions } from '../store/Common.reducer'
+import { useCallback } from 'react'
 
 export const useAllBridgeTokens = () => {
   const { chainId } = useActiveWeb3React()
@@ -20,4 +22,15 @@ export const useAllBridgeLists = () => {
   const allLists = useSelector(selectAllLists)
 
   return allLists
+}
+
+export const useActiveListsHandlers = () => {
+  const dispatch = useDispatch()
+  const activeLists = useSelector((state: AppState) => state.omnibridge.common.activeLists)
+
+  return {
+    activateList: useCallback((listId: string) => dispatch(commonActions.activateLists([listId])), [dispatch]),
+    deactivateList: useCallback((listId: string) => dispatch(commonActions.deactivateLists([listId])), [dispatch]),
+    isListActive: useCallback((listId: string) => activeLists.includes(listId), [activeLists])
+  }
 }
