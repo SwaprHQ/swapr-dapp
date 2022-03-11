59802cd526fd741e6f9861e28ea4ea912b0d0dfc
mix1o <smgpwwe@wp.pl>
2022-03-11 12:13:26 +0100

    export helpers

--- src/services/Omnibridge/Arbitrum/ArbitrumBridge.utils.ts
+++ src/services/Omnibridge/Arbitrum/ArbitrumBridge.utils.ts
@@ -3,6 +3,8 @@ import { AppState } from '../../../state'
 import { createArbitrumSlice } from './ArbitrumBridge.reducer'
 import { clearBridgeTxs } from '../../../state/bridgeTransactions/actions'
 import { SupportedChainsConfig } from '../Omnibridge.types'
+import { gql } from 'graphql-request'
+import { BigNumber } from 'ethers'
 
 export const migrateBridgeTransactions = (
   store: Store<AppState>,
@@ -20,3 +22,20 @@ export const migrateBridgeTransactions = (
   store.dispatch(actions.migrateTxs({ [from]: fromTxs, [to]: toTxs }))
   store.dispatch(clearBridgeTxs([from, to]))
 }
+
+export const QUERY_ETH_PRICE = gql`
+  query {
+    bundle(id: "1") {
+      nativeCurrencyPrice
+    }
+  }
+`
+
+export const getErrorMsg = (error: any) => {
+  if (error?.code === 4001) {
+    return 'Transaction rejected'
+  }
+  return `Bridge failed: ${error.message}`
+}
+
+export const MAX_SUBMISSION_PRICE_PERCENT_INCREASE = BigNumber.from(400)
