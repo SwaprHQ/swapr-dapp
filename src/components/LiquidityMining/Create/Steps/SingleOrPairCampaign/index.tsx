be2e652111f2a44ba528b76baa34c1a40e941b3c
Violette <milanamsterdam@Milans-MacBook-Pro.local>
2022-03-18 16:34:46 +0100

    feature: Adjustable circle icon

--- src/components/LiquidityMining/Create/Steps/SingleOrPairCampaign/index.tsx
+++ src/components/LiquidityMining/Create/Steps/SingleOrPairCampaign/index.tsx
@@ -14,7 +14,7 @@ const Card = styled(StyleCar)<{ active?: boolean }>`
   opacity: ${props => (!props.selectable || props.active ? '1' : '0.4')};
   cursor: pointer;
   ::before {
-    border: 1px solid;
+    border: 1px solid #464366;
     backdrop-filter: blur(20px);
     border-image-source: ${props =>
       props.active ? 'linear-gradient(114.28deg, rgba(36, 23, 137, 0.2) 0%, #282167 91.9%)' : 'transperent'};
@@ -40,16 +40,20 @@ const CardText = styled(Text)`
   text-transform: uppercase;
 `
 
-const Diamond = styled.div<{ size: string }>`
+const Diamond = styled.div<{ size: string; active: boolean }>`
   position: absolute;
   width: ${props => props.size}px;
   height: ${props => props.size}px;
+  border: ${props => (props.active ? 'none' : '1px solid #2A2F42')};
+  background: ${props =>
+    props.active ? 'linear-gradient(135deg, rgba(118, 52, 255, 0.7) 16.33%, rgba(35, 0, 102, 0) 95.92%)' : '#3C38641A'};
+  box-shadow: ${props =>
+    props.active
+      ? 'inset 5.17256px 8.62093px 25.8628px rgba(255, 255, 255, 0.16), inset 9.56367px 3.18789px 15.9394px rgba(255, 255, 255, 0.1);'
+      : 'inset 0px 1.55659px 9.33954px rgba(165, 164, 255, 0.08),  inset 9.33954px 3.11318px 15.5659px rgba(143, 141, 255, 0.1);'};
 
-  background: linear-gradient(135deg, rgba(118, 52, 255, 0.7) 16.33%, rgba(35, 0, 102, 0) 95.92%);
-  box-shadow: inset 5.17256px 8.62093px 25.8628px rgba(255, 255, 255, 0.16),
-    inset 9.56367px 3.18789px 15.9394px rgba(255, 255, 255, 0.1);
   filter: drop-shadow(18px 0px 60px rgba(175, 135, 255, 0.3));
-  backdrop-filter: blur(22.4144px);
+  backdrop-filter: ${props => (props.active ? 'blur(22.4144px)' : 'blur(14px)')};
 
   border-radius: 255.031px;
 `
@@ -57,6 +61,17 @@ const Diamond = styled.div<{ size: string }>`
 interface SingleOrPairCampaignProps {
   singleReward: CampaignType
   onChange: (newValue: CampaignType) => void
+}
+
+export const adjustableDiamondSize = (isSingle: boolean, active: boolean) => {
+  if (isSingle) return <Diamond active={active} size={'98'} style={{ top: '21px', left: '-27px' }} />
+  else
+    return (
+      <>
+        <Diamond active={active} style={{ top: '26px', left: '-38px' }} size={'84'} />
+        <Diamond active={active} style={{ top: '26px', left: '0px' }} size={'84'} />
+      </>
+    )
 }
 
 export default function SingleOrPairCampaign({ singleReward, onChange }: SingleOrPairCampaignProps) {
@@ -71,7 +86,7 @@ export default function SingleOrPairCampaign({ singleReward, onChange }: SingleO
   return (
     <AutoRow gap="35px">
       <Card active={singleReward === CampaignType.TOKEN} onClick={() => handleRewardClick(CampaignType.TOKEN)}>
-        <Diamond size={'98'} style={{ top: '21px', left: '-27px' }} />
+        {adjustableDiamondSize(true, singleReward === CampaignType.TOKEN)}
         <AutoColumn>
           <CardText>Single</CardText>
           <CardText>Token</CardText>
@@ -79,8 +94,7 @@ export default function SingleOrPairCampaign({ singleReward, onChange }: SingleO
         </AutoColumn>
       </Card>
       <Card onClick={() => handleRewardClick(CampaignType.PAIR)} active={singleReward === CampaignType.PAIR}>
-        <Diamond style={{ top: '26px', left: '-38px' }} size={'84'} />
-        <Diamond style={{ top: '26px', left: '0px' }} size={'84'} />
+        {adjustableDiamondSize(false, singleReward === CampaignType.PAIR)}
         <AutoColumn>
           <CardText>LP</CardText>
           <CardText>Token</CardText>
