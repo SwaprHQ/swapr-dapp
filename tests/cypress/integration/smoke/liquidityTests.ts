951ca0357f215b55ce3911df89c3701be353cee8
kacper29 <niemam29@gmail.com>
2022-03-31 18:11:57 +0200

    Merging rewards tests

--- tests/cypress/integration/smoke/liquidityTests.ts
+++ tests/cypress/integration/smoke/liquidityTests.ts
@@ -6,23 +6,23 @@ describe('Check Liquidity Page', () => {
   })
 
   it('Check All Pairs button and campaigns/my pairs toggle switch on Liquidity Page', () => {
-    LiquidityPage.allPairs_Button().should('be.visible')
-    LiquidityPage.campaignsAndMyPairs_ToggleSwitch().should('be.visible')
+    LiquidityPage.allPairsButton().should('be.visible')
+    LiquidityPage.campaignsAndMyPairsToggleSwitch().should('be.visible')
   })
 
   it('Check Create Pair button and modal window on Liquidity Page', () => {
-    LiquidityPage.createPair_Button().should('be.visible')
-    LiquidityPage.createPair_Button().click()
+    LiquidityPage.createPairButton().should('be.visible')
+    LiquidityPage.createPairButton().click()
     LiquidityPage.inputFields().should('be.visible')
-    LiquidityPage.selectToken_Button().should('be.visible')
+    LiquidityPage.selectTokenButton().should('be.visible')
   })
 
   it('Check Select Token modal window on Liquidity Page', () => {
-    LiquidityPage.createPair_Button().click()
-    LiquidityPage.createAPairInput_Field().should('be.visible')
-    LiquidityPage.selectToken_Button()
+    LiquidityPage.createPairButton().click()
+    LiquidityPage.createAPairInputField().should('be.visible')
+    LiquidityPage.selectTokenButton()
       .first()
       .click()
-    LiquidityPage.tokenSearch_Field().should('be.visible')
+    LiquidityPage.tokenSearchField().should('be.visible')
   })
 })
