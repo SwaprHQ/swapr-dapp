872a4b3ae6d5104603362e41db874481af2d6a82
MilanVojnovic95 <mv.coversproduction@gmail.com>
2022-04-07 22:07:32 +0200

    Rewards Page Smoke Test / applied prettier and fixed naming convention for selectors

--- tests/pages/RewardsPage.ts
+++ tests/pages/RewardsPage.ts
@@ -2,31 +2,31 @@ export class RewardsPage {
   static visitSwapPage() {
     cy.visit('/rewards?chainId=100')
   }
-  static activeCampaigns_Button() {
-    return  cy.get('[data-testid=active-campaigns]')
+  static getActiveCampaignsButton() {
+    return cy.get('[data-testid=active-campaigns]')
   }
-  static expiredCampaigns_Button() {
-    return  cy.get('[data-testid=expired-campaigns]')
+  static getExpiredCampaignsButton() {
+    return cy.get('[data-testid=expired-campaigns]')
   }
-  static createCampaign_Button() {
-    return  cy.get('[data-testid=create-campaign]')
+  static getCreateCampaignButton() {
+    return cy.get('[data-testid=create-campaign]')
   }
-  static allPairs_Button() {
-    return  cy.get('[data-testid=all-pairs]')
+  static getAllPairsButton() {
+    return cy.get('[data-testid=all-pairs]')
   }
-  static searchAPair_ModalWindow() {
-    return  cy.get('[data-testid=select-a-pair]')
+  static getSearchAPairModalWindow() {
+    return cy.get('[data-testid=select-a-pair]')
   }
-  static searchAPair_Field() {
-    return  cy.get('[data-testid=search-pair]')
+  static getSearchAPairField() {
+    return cy.get('[data-testid=search-pair]')
   }
-  static closeSearchAPairModalWindow_Button() {
-    return  cy.get('[data-testid=close-search-pair]')
+  static getCloseSearchAPairModalWindowButton() {
+    return cy.get('[data-testid=close-search-pair]')
   }
-  static myPairs_ToggleSwitch() {
-    return  cy.get('.react-switch-button')
+  static getMyPairsToggleSwitch() {
+    return cy.get('.react-switch-button')
   }
-  static rewardCard() {
-    return  cy.get('[data-testid=ended-campaign]')
+  static getRewardCard() {
+    return cy.get('[data-testid=ended-campaign]')
   }
-}  
+}
