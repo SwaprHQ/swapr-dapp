e13f5ad49539936fdefd99528c33b88563908733
Adam Azad <adam@adamazad.com>
2022-04-18 10:29:20 +0200

    fix: remove whitespace

--- src/components/Web3Status/AccountStatus.tsx
+++ src/components/Web3Status/AccountStatus.tsx
@@ -175,7 +175,6 @@ export function AccountStatus({
           ) : ENSName ? (
             <>
               {avatar?.image && <Avatar url={avatar.image} />}
-
               <p>{ENSName}</p>
             </>
           ) : (
