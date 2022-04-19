63d92b048e1679712c4f5335e8d54c902094f304
Daniel Cybułka <danielcybulka@gmail.com>
2022-04-19 10:52:17 +0200

    Update src/services/EcoBridge/Socket/Socket.types.ts
    Co-authored-by: Jorge Lopes <36545016+jorgedclopes@users.noreply.github.com>

--- src/services/EcoBridge/Socket/Socket.types.ts
+++ src/services/EcoBridge/Socket/Socket.types.ts
@@ -58,8 +58,5 @@ export interface SocketBridgeState {
 type UserTxs = [{ steps: [{ protocolFees: { amount: string; feesInUsd: number; asset: { decimals: number } } }] }]
 
 export function isFee(userTxs: any): userTxs is UserTxs {
-  if (userTxs.length && userTxs[0].steps.length && userTxs[0].steps) {
-    return true
-  }
-  return false
+  return (userTxs.length && userTxs[0].steps.length && userTxs[0].steps)
 }
