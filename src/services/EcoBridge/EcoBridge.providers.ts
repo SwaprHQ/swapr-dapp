93b8040f29636db7335d99492005ad3832cd13c3
mix1o <smgpwwe@wp.pl>
2022-04-19 16:47:55 +0200

    deleted unused code

--- src/services/EcoBridge/EcoBridge.providers.ts
+++ src/services/EcoBridge/EcoBridge.providers.ts
@@ -6,13 +6,7 @@ import { EcoBridgeProviders } from './EcoBridge.types'
 
 const addInfuraKey = (rpcUrl: string) => {
   if (rpcUrl.includes('infura')) {
-    let updatedUrl = rpcUrl
-
-    if (!rpcUrl.endsWith('/')) {
-      updatedUrl = rpcUrl + '/'
-    }
-
-    return updatedUrl + INFURA_PROJECT_ID
+    return rpcUrl.endsWith('/') ? `${rpcUrl}${INFURA_PROJECT_ID}` : `${rpcUrl}/${INFURA_PROJECT_ID}`
   }
 
   return rpcUrl
@@ -22,10 +16,6 @@ export const initiateEcoBridgeProviders = (): EcoBridgeProviders => ({
   [ChainId.MAINNET]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.MAINNET].rpcUrls[0])),
   [ChainId.ARBITRUM_ONE]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.ARBITRUM_ONE].rpcUrls[0])),
   [ChainId.ARBITRUM_RINKEBY]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.ARBITRUM_RINKEBY].rpcUrls[0])),
-  // [ChainId.BSC]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.BSC].rpcUrls[0])),
-  // [ChainId.BSC_TESTNET]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.BSC_TESTNET].rpcUrls[0])),
-  // [ChainId.KOVAN]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.KOVAN].rpcUrls[0])),
   [ChainId.RINKEBY]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.RINKEBY].rpcUrls[0])),
-  // [ChainId.SOKOL]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.SOKOL].rpcUrls[0])),
   [ChainId.XDAI]: new JsonRpcProvider(addInfuraKey(NETWORK_DETAIL[ChainId.XDAI].rpcUrls[0]))
 })
