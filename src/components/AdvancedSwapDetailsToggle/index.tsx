905d5f2989d1f033abda95cfbd2430e31dcfa90a
Berteotti <berteotti2@gmail.com>
2022-04-19 10:35:48 +0100

    Add FC type to AdvancedSwapDetailsToggle

--- src/components/AdvancedSwapDetailsToggle/index.tsx
+++ src/components/AdvancedSwapDetailsToggle/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { FC } from 'react'
 import styled from 'styled-components'
 import { ChevronDown, ChevronUp } from 'react-feather'
+import { Flex } from 'rebass'
 
-const StyledAdvancedSwapDetailsButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const StyledAdvancedSwapDetailsButton = styled(Flex)`
   cursor: pointer;
   background: rgba(104, 110, 148, 0.3);
   border-radius: 4px;
@@ -13,15 +11,16 @@ const StyledAdvancedSwapDetailsButton = styled.div`
   width: 22px;
 `
 
-export const AdvancedSwapDetailsToggle = ({
-  showAdvancedSwapDetails,
-  setShowAdvancedSwapDetails
-}: {
+export const AdvancedSwapDetailsToggle: FC<{
   showAdvancedSwapDetails: boolean
   setShowAdvancedSwapDetails: (value: boolean) => void
-}) => {
+}> = ({ showAdvancedSwapDetails, setShowAdvancedSwapDetails }) => {
   return (
-    <StyledAdvancedSwapDetailsButton onClick={() => setShowAdvancedSwapDetails(!showAdvancedSwapDetails)}>
+    <StyledAdvancedSwapDetailsButton
+      justifyContent="center"
+      alignItems="center"
+      onClick={() => setShowAdvancedSwapDetails(!showAdvancedSwapDetails)}
+    >
       {showAdvancedSwapDetails ? <ChevronUp size={14} /> : <ChevronDown size={14} />}
     </StyledAdvancedSwapDetailsButton>
   )
