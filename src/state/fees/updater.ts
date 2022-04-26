24ba4f9dcef21435b02333e724727b59eade388d
Daniel Cybulka <danielcybulka@gmail.com>
2022-04-26 09:44:36 +0200

    disable fee updater when on unsupported chain

--- src/state/fees/updater.ts
+++ src/state/fees/updater.ts
@@ -3,16 +3,19 @@ import { useActiveWeb3React } from '../../hooks'
 import { setSwapFees, setProtocolFee } from './actions'
 import { useDispatch } from 'react-redux'
 import { Fetcher } from '@swapr/sdk'
+import { chainSupportsSWPR } from '../../utils/chainSupportsSWPR'
 
 export default function Updater() {
   const { library, chainId } = useActiveWeb3React()
   const dispatch = useDispatch()
 
+  const isSWPRSupportedChain = chainSupportsSWPR(chainId)
+
   useEffect(() => {
-    if (library && chainId)
+    if (library && chainId && isSWPRSupportedChain)
       Promise.all([
         Fetcher.fetchAllSwapFees(chainId, {}, library as any),
-        Fetcher.fetchProtocolFee(chainId, library as any),
+        Fetcher.fetchProtocolFee(chainId, library as any)
       ])
         .then(([swapFees, protocolFee]) => {
           if (swapFees) dispatch(setSwapFees({ swapFees }))
@@ -20,7 +23,7 @@ export default function Updater() {
             dispatch(
               setProtocolFee({
                 protocolFeeDenominator: Number(protocolFee.feeDenominator) + 1,
-                protocolFeeTo: protocolFee.feeReceiver,
+                protocolFeeTo: protocolFee.feeReceiver
               })
             )
         })
@@ -28,7 +31,7 @@ export default function Updater() {
           console.error('Cancelled fetch for fees, error:', error)
           return
         })
-  }, [library, chainId, dispatch])
+  }, [library, chainId, dispatch, isSWPRSupportedChain])
 
   return null
 }
