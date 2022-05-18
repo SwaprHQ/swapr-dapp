643104aae841dfb637c6c2c10294ea0c6a4e538c
Mi-Lan <git>
2022-05-18 20:18:14 +0200

    feature: Added dynamic dispaly of price and amount simulation along with icons and styles

--- src/components/LiquidityMining/Create/Steps/PreviewAndCreate/SimulateStaking/index.tsx
+++ src/components/LiquidityMining/Create/Steps/PreviewAndCreate/SimulateStaking/index.tsx
@@ -1,16 +1,21 @@
 import React, { useCallback, useMemo, useState } from 'react'
 
-import { TYPE } from '../../../../../../theme'
 import { SmoothGradientCard } from '../../../../styleds'
 import styled from 'styled-components'
 import Loader from '../../../../../Loader'
 import Slider from '../../../../../Slider'
-import { Repeat } from 'react-feather'
+
 import useDebouncedChangeHandler from '../../../../../../utils/useDebouncedChangeHandler'
 import { Pair, Price, Token, TokenAmount } from '@swapr/sdk'
 import { useTokenOrPairNativeCurrency } from '../../../../../../hooks/useTokenOrPairNativeCurrency'
 import { parseUnits } from 'ethers/lib/utils'
 import { calculatePercentage } from '../../../../../../utils'
+import { Flex } from 'rebass'
+import { ReactComponent as CashIcon } from '../../../../../../assets/svg/cash-icon.svg'
+import { ReactComponent as CryptoIcon } from '../../../../../../assets/svg/crypto-icon.svg'
+import { ReactComponent as RefreshIcon } from '../../../../../../assets/svg/refresh-icon.svg'
+import NumericalInput from '../../../../../Input/NumericalInput'
+import { TYPE } from '../../../../../../theme'
 
 const SwitchContainer = styled.div`
   font-size: 10px;
@@ -21,6 +26,9 @@ const SwitchContainer = styled.div`
   letter-spacing: 0.15em;
   text-transform: uppercase;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 4px;
 `
 const SimulatedValue = styled.div`
   font-family: 'Fira Mono';
@@ -36,12 +44,32 @@ const SimulatedValue = styled.div`
 
   color: ${props => props.theme.text2};
 `
-const StyledSwitch = styled(Repeat)`
-  width: 12px;
-  height: 12px;
-  stroke: ${props => props.theme.text5};
-  margin-left: 4px;
+const AmountFlex = styled(Flex)`
+  height: 28px;
+  width: max-content;
+  align-self: center;
 `
+
+const SimulateOption = styled.div<{ isActive: boolean }>`
+  font-size: 11px;
+  font-weight: 600;
+  letter-spacing: 0.08em;
+  color: ${props => (props.isActive ? props.theme.text2 : props.theme.lightPurple2)};
+  ${props => props.isActive && 'text-decoration: underline; text-underline-offset: 7px;'};
+`
+const StyledNumericalInput = styled(NumericalInput)`
+  color: ${props => props.theme.text2};
+  width: 36px;
+  max-height: 38px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 16px;
+  text-transform: uppercase;
+  ::placeholder {
+    color: ${props => props.theme.text2};
+  }
+`
+
 const dollarAmountMaxSimulation = 10000000
 
 interface RewardSummaryProps {
@@ -51,6 +79,10 @@ interface RewardSummaryProps {
   setSimulatedStakedAmount: (value: string) => void
   loading: boolean
 }
+enum SimulateOptions {
+  AMOUNT = 'Amount',
+  PRICE = 'Price',
+}
 
 export default function SimulateStaking({
   tokenOrPair,
@@ -59,9 +91,27 @@ export default function SimulateStaking({
   nativeCurrencyUSDPrice,
   loading,
 }: RewardSummaryProps) {
+  const [simulatedPrice, setSimulatedPrice] = useState('')
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const widthValue = useMemo(() => {
+    if (simulatedPrice.length > 0 && inputRef.current) return inputRef.current.clientWidth
+    else return 18
+  }, [simulatedPrice, inputRef])
+  const handleLocalStakingCapChange = useCallback(
+    rawValue => {
+      if (!tokenOrPair || (tokenOrPair instanceof Pair && !tokenOrPair.liquidityToken)) return
+      setSimulatedPrice(rawValue)
+      //   const tokenOrPair = stakeTokenOrPair instanceof Token ? tokenOrPair : tokenOrPair.liquidityToken
+      //   const parsedAmount = tryParseAmount(rawValue, tokenOrPair) as TokenAmount | undefined
+      //   onStakingCapChange(parsedAmount || new TokenAmount(tokenOrPair, '0'))
+    },
+    [tokenOrPair]
+  )
+
   const { loading: loadingNativeTokenPrice, derivedNativeCurrency: nativeTokenPrice } = useTokenOrPairNativeCurrency(
     tokenOrPair ? tokenOrPair : undefined
   )
+  const [simulateOption, setSimulateOption] = useState<SimulateOptions>(SimulateOptions.AMOUNT)
   const [showUSDValue, setShowUSDValue] = useState(true)
   const [simulatedValuePercentage, setSimulatedValuePercentage] = useState(0)
   const liquidityPercentChangeCallback = useCallback((value: number) => {
@@ -73,6 +123,7 @@ export default function SimulateStaking({
     10
   )
   const maxStakedSimulatedAmount = useMemo(() => {
+    //TODO : add value from input inside here so we always have a max value
     const base = stakingCap
       ? parseFloat(stakingCap.multiply(nativeTokenPrice.multiply(nativeCurrencyUSDPrice)).toSignificant(22))
       : dollarAmountMaxSimulation
@@ -113,27 +164,69 @@ export default function SimulateStaking({
       padding={'24px 28px'}
       width={'50%'}
     >
-      <TYPE.largeHeader fontSize={'11px'} letterSpacing="0.08em" color="text3">
-        SIMULATED STAKED AMOUNT
-      </TYPE.largeHeader>
-      <SwitchContainer onClick={handleUSDValueClick}>
-        Value in {showUSDValue ? 'crypto' : 'USD'}
-        <StyledSwitch />
-      </SwitchContainer>
-      {loading || loadingNativeTokenPrice ? (
-        <Loader />
-      ) : (
-        <SimulatedValue>
-          {maxStakedSimulatedAmount.toLocaleString('en-us')}{' '}
-          {showUSDValue
-            ? 'USD'
-            : tokenOrPair instanceof Token
-            ? tokenOrPair.symbol
-            : `${tokenOrPair?.token0.symbol}/${tokenOrPair?.token1.symbol}`}
-        </SimulatedValue>
+      <Flex width={'100%'} justifyContent={'space-between'}>
+        <SimulateOption
+          onClick={() => setSimulateOption(SimulateOptions.AMOUNT)}
+          isActive={simulateOption === SimulateOptions.AMOUNT}
+        >
+          SIMULATED STAKING
+        </SimulateOption>
+        <SimulateOption
+          onClick={() => setSimulateOption(SimulateOptions.PRICE)}
+          isActive={simulateOption === SimulateOptions.PRICE}
+        >
+          SIMULATED PRICE
+        </SimulateOption>
+      </Flex>
+
+      {SimulateOptions.AMOUNT === simulateOption && (
+        <>
+          {loading || loadingNativeTokenPrice ? (
+            <Loader />
+          ) : (
+            <SimulatedValue>
+              {maxStakedSimulatedAmount.toLocaleString('en-us')}{' '}
+              {showUSDValue
+                ? 'USD'
+                : tokenOrPair instanceof Token
+                ? tokenOrPair.symbol
+                : `${tokenOrPair?.token0.symbol}/${tokenOrPair?.token1.symbol}`}
+            </SimulatedValue>
+          )}
+          <Slider value={innerLiquidityPercentage} size={16} onChange={setInnerLiquidityPercentage} />
+        </>
+      )}
+      {SimulateOptions.PRICE === simulateOption && (
+        <AmountFlex>
+          <StyledNumericalInput
+            style={{ width: widthValue + 12 + 'px' }}
+            value={simulatedPrice}
+            onUserInput={handleLocalStakingCapChange}
+          />
+          <span style={{ visibility: 'hidden', position: 'absolute' }} ref={inputRef}>
+            {simulatedPrice}
+          </span>
+
+          <TYPE.largeHeader
+            alignSelf={'center'}
+            fontSize={13}
+            color={'text3'}
+            letterSpacing="0.08em"
+            alignItems={'center'}
+          >
+            USD
+          </TYPE.largeHeader>
+        </AmountFlex>
       )}
 
-      <Slider value={innerLiquidityPercentage} size={16} onChange={setInnerLiquidityPercentage} />
+      <Flex width={'100%'} justifyContent={'space-between'}>
+        <SwitchContainer onClick={handleUSDValueClick}>
+          {showUSDValue ? <CashIcon /> : <CryptoIcon />} SHOW IN {showUSDValue ? 'CRYPTO' : 'USD'}
+        </SwitchContainer>
+        <SwitchContainer>
+          RESET <RefreshIcon />
+        </SwitchContainer>
+      </Flex>
     </SmoothGradientCard>
   )
 }
