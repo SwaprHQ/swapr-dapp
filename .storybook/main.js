f4e10a5aa69a90717c84371797156bbc5537e193
Mi-Lan <git>
2022-06-27 12:35:12 +0200

    fix: MDX support

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
     '@react-theming/storybook-addon',
+    '@storybook/addon-docs',
   ],
   framework: '@storybook/react',
 }
