f07e5b25215c99e17d6ebe07c5d04f8228e511f0
Wixzi <wixzi@pm.me>
2022-07-14 15:27:03 +0200

    Update storybook to webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,4 +9,26 @@ module.exports = {
     '@storybook/addon-docs',
   ],
   framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async config => {
+    const fallback = config.resolve.fallback || {}
+    Object.assign(fallback, {
+      fs: false,
+      tls: require.resolve('tls'),
+      net: require.resolve('net'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      url: require.resolve('url'),
+      path: require.resolve('path-browserify'),
+    })
+    config.resolve.fallback = fallback
+
+    return config
+  },
 }
