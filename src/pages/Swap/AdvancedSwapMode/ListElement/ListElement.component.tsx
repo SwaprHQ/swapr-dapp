211062030c16d4840849a2b4699244a990426807
mix1o <smgpwwe@wp.pl>
2022-07-28 15:22:47 +0200

    add formatDate function to ListElement

--- src/pages/Swap/AdvancedSwapMode/ListElement/ListElement.component.tsx
+++ src/pages/Swap/AdvancedSwapMode/ListElement/ListElement.component.tsx
@@ -1,10 +1,20 @@
+import { formatDistance, subDays } from 'date-fns'
 import React from 'react'
 
-import { formatDate } from '../AdvancedSwapMode.utils'
 import { Cell, Root } from './ListElement.styles'
 import { ListElementProps } from './ListElement.types'
 
 export const ListElement = ({ amountUSD, amount0, amount1, timestamp }: ListElementProps) => {
+  const formatDate = (timestamp: number) => {
+    try {
+      return formatDistance(subDays(new Date(timestamp), 3), new Date(), {
+        addSuffix: true,
+      })
+    } catch {
+      return '-'
+    }
+  }
+
   return (
     <Root>
       <Cell>{Number(amountUSD).toFixed(5)}</Cell>
