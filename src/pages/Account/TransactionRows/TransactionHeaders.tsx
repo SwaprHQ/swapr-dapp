90a6d38f410677de58bc7acfdf24d52aa2c2add0
Wixzi <wixzi@pm.me>
2022-08-26 16:59:35 +0200

    Update Transaction Headers

--- src/pages/Account/TransactionRows/TransactionHeaders.tsx
+++ src/pages/Account/TransactionRows/TransactionHeaders.tsx
@@ -1,5 +1,3 @@
-import { Flex } from 'rebass'
-
 import { Header } from '../../../ui/Header'
 import { HeaderText } from '../../../ui/HeaderText'
 import { HeaderRow, TranasctionDetails } from '../Account.styles'
@@ -9,20 +7,16 @@ export function TransactionHeaders() {
     <HeaderRow>
       <HeaderText>
         <Header justifyContent="space-between" paddingX="22px" paddingY="12px">
-          <Flex flex="55%">
-            <TranasctionDetails flex="15%" justifyContent="start">
-              From
-            </TranasctionDetails>
-            <TranasctionDetails flex="15%" justifyContent="start">
-              To
-            </TranasctionDetails>
-            <TranasctionDetails justifyContent="start">Price</TranasctionDetails>
-          </Flex>
-          <Flex flex="45%">
-            <TranasctionDetails>Type</TranasctionDetails>
-            <TranasctionDetails>Status</TranasctionDetails>
-            <TranasctionDetails>Time</TranasctionDetails>
-          </Flex>
+          <TranasctionDetails flex="15%" justifyContent="start">
+            From
+          </TranasctionDetails>
+          <TranasctionDetails flex="15%" justifyContent="start">
+            To
+          </TranasctionDetails>
+          <TranasctionDetails justifyContent="start">Price</TranasctionDetails>
+          <TranasctionDetails>Type</TranasctionDetails>
+          <TranasctionDetails>Status</TranasctionDetails>
+          <TranasctionDetails>Time</TranasctionDetails>
         </Header>
       </HeaderText>
     </HeaderRow>
