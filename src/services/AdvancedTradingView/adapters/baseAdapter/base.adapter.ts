c2f2cb81415295e27dcca52e37fa4ce07386c6a2
mix1o <smgpwwe@wp.pl>
2022-08-31 14:34:36 +0200

    make base adapter more generic

---
+++ src/services/AdvancedTradingView/adapters/baseAdapter/base.adapter.ts
@@ -0,0 +1,151 @@
+import { ChainId, Pair, Token, UniswapV2RoutablePlatform } from '@swapr/sdk'
+
+import { request, RequestOptions } from 'graphql-request'
+
+import { actions } from '../../advancedTradingView.reducer'
+import {
+  AdapterFetchDetails,
+  AdapterInitialArguments,
+  AdapterKeys,
+  AdapterPayloadType,
+} from '../../advancedTradingView.types'
+import { AbstractAdvancedTradingViewAdapter } from '../advancedTradingView.adapter'
+import { PAIR_BURNS_AND_MINTS, PAIR_SWAPS } from './base.queries'
+import { PairBurnsAndMints, PairSwaps } from './base.types'
+
+export class BaseAdapter extends AbstractAdvancedTradingViewAdapter {
+  private _key: AdapterKeys
+  private _adapterSupportedChains: ChainId[]
+  private _platform: UniswapV2RoutablePlatform
+  private _subgraphUrls: { [key in ChainId.MAINNET | ChainId.ARBITRUM_ONE | ChainId.GNOSIS]: string }
+
+  constructor({
+    key,
+    adapterSupportedChains,
+    platform,
+    subgraphUrls,
+  }: {
+    key: AdapterKeys
+    adapterSupportedChains: ChainId[]
+    platform: UniswapV2RoutablePlatform
+    subgraphUrls: { [key in ChainId.MAINNET | ChainId.ARBITRUM_ONE | ChainId.GNOSIS]: string }
+  }) {
+    super()
+
+    this._key = key
+    this._platform = platform
+    this._subgraphUrls = subgraphUrls
+    this._adapterSupportedChains = adapterSupportedChains
+  }
+
+  public updateActiveChainId(chainId: ChainId) {
+    this._chainId = chainId
+  }
+
+  public setInitialArguments({ chainId, store }: AdapterInitialArguments) {
+    this._chainId = chainId
+    this._store = store
+  }
+
+  public async getPairTrades({
+    inputToken,
+    outputToken,
+    amountToFetch,
+    isFirstFetch,
+    abortController,
+  }: AdapterFetchDetails) {
+    if (!this._isSupportedChainId(this._chainId)) return
+
+    const subgraphPairId = this._getSubgraphPairId(inputToken, outputToken)
+
+    const pair = this.store.getState().advancedTradingView.adapters[this._key][subgraphPairId]
+
+    if ((pair && !isFirstFetch && !pair.swaps?.hasMore) || (pair && isFirstFetch)) return
+
+    try {
+      const { swaps } = await request<PairSwaps>({
+        url: this._subgraphUrls[this._chainId],
+        document: PAIR_SWAPS,
+        variables: {
+          pairId: subgraphPairId,
+          first: amountToFetch,
+          skip: pair?.swaps?.data.length ?? 0,
+        },
+        signal: abortController(`${this._key}-pair-trades`) as RequestOptions['signal'],
+      })
+
+      const hasMore = swaps.length === amountToFetch
+
+      this.store.dispatch(
+        this.actions.setPairData({
+          key: this._key,
+          pairId: subgraphPairId,
+          payloadType: AdapterPayloadType.swaps,
+          data: swaps,
+          hasMore,
+        })
+      )
+    } catch {}
+  }
+
+  public async getPairActivity({
+    inputToken,
+    outputToken,
+    amountToFetch,
+    isFirstFetch,
+    abortController,
+  }: AdapterFetchDetails) {
+    if (!this._isSupportedChainId(this._chainId)) return
+
+    const subgraphPairId = this._getSubgraphPairId(inputToken, outputToken)
+
+    const pair = this.store.getState().advancedTradingView.adapters[this._key][subgraphPairId]
+
+    if ((pair && !isFirstFetch && !pair.burnsAndMints?.hasMore) || (pair && isFirstFetch)) return
+
+    try {
+      const { burns, mints } = await request<PairBurnsAndMints>({
+        url: this._subgraphUrls[this._chainId],
+        document: PAIR_BURNS_AND_MINTS,
+        variables: {
+          pairId: subgraphPairId,
+          first: amountToFetch,
+          skip: pair?.burnsAndMints?.data.length ?? 0,
+        },
+        signal: abortController(`${this._key}-pair-activity`) as RequestOptions['signal'],
+      })
+
+      const hasMore = Boolean(burns.length === amountToFetch || mints.length === amountToFetch)
+
+      this.store.dispatch(
+        this.actions.setPairData({
+          key: this._key,
+          pairId: subgraphPairId,
+          payloadType: AdapterPayloadType.burnsAndMints,
+          data: [...burns, ...mints],
+          hasMore,
+        })
+      )
+    } catch {}
+  }
+
+  private get actions() {
+    return actions
+  }
+
+  private get store() {
+    if (!this._store) throw new Error('No store set')
+
+    return this._store
+  }
+
+  private _getSubgraphPairId(inputToken: Token, outputToken: Token) {
+    return Pair.getAddress(inputToken, outputToken, this._platform).toLowerCase()
+  }
+
+  private _isSupportedChainId(chainId?: ChainId): chainId is ChainId.MAINNET | ChainId.GNOSIS | ChainId.ARBITRUM_ONE {
+    if (!chainId) return false
+
+    return this._adapterSupportedChains.includes(chainId)
+  }
+}
