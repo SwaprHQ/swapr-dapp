4ed2be3a01e8d0fafde83f42caad565bcec9c7a2
kacper29 <niemam29@gmail.com>
2022-09-06 15:17:43 +0200

    fixes from cr trigger-synpress skip-cypress

--- tests/synpress/specs/02transactionfull/rewards/CreatingCampaignTests.ts
+++ tests/synpress/specs/02transactionfull/rewards/CreatingCampaignTests.ts
@@ -88,7 +88,7 @@ describe('Campaign creation tests', () => {
     TokenMenu.searchAndChooseToken('dai')
     LiquidityPage.getPairCards().contains('USDT').click({})
     LiquidityPage.getOpenRewardsButton().should(button => {
-      let rewardAmount = parseInt(button.text().replace(/\D+/g, ''))
+      const rewardAmount = parseInt(button.text().replace(/\D+/g, ''))
       expect(rewardAmount).to.be.greaterThan(0)
     })
     RewardsPage.getRewardCardByStartingAt(getUnixTime(expectedStartsAt).toString()).click({})
