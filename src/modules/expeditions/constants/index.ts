0a20cac4e35debba602fd8da24caf413072c41f2
Adam Azad <adam@adamazad.com>
2022-09-06 12:16:50 +0100

    feat(#1368): add local overrides

--- src/modules/expeditions/constants/index.ts
+++ src/modules/expeditions/constants/index.ts
@@ -1,6 +1,6 @@
 import { ClaimWeeklyFragmentsTypeEnum } from '../api/generated'
 
-const apiBaseURLDev = 'http://localhost:4000'
+const apiBaseURLDev = process.env.REACT_APP_SWAPR_API_BASE_URL || 'https://api-dev.swapr.site/v1.0'
 const apiBaseURLProd = 'https://api.swapr.site/v1.0'
 
 /**
