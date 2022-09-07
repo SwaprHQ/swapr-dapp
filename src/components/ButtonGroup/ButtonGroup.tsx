0d1b225b4f09716d360b23f5c21c540320f5fad9
Diogo <diogomf@hey.com>
2022-09-07 16:48:04 +0100

    Simplify ButtonGroup, remove complex logic and add css

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -4,8 +4,6 @@ import styled from 'styled-components'
 
 export const ButtonGroupOption = styled.button<{
   active?: boolean
-  first?: boolean
-  last?: boolean
 }>`
   display: flex;
   justify-content: center;
@@ -20,7 +18,14 @@ export const ButtonGroupOption = styled.button<{
 
   background: ${({ active }) => (active ? 'rgba(104, 110, 148, 0.25)' : 'rgba(62, 65, 87, 0.2)')};
   border: 1px solid #2c2b42;
-  border-radius: ${({ first, last }) => (first ? '8px 0px 0px 8px' : last ? '0 8px 8px 0' : '0')};
+
+  &:first-child {
+    border-radius: 8px 0px 0px 8px;
+  }
+
+  &:last-child {
+    border-radius: 0 8px 8px 0;
+  }
 
   &:hover {
     color: ${({ theme }) => theme.text4};
@@ -35,18 +40,5 @@ export const ButtonGroupOption = styled.button<{
 `
 
 export const ButtonGroup = ({ children }: { children: ReactNode }) => {
-  const childrenWithProps = React.Children.map(children, (child, index) => {
-    let props = {
-      last: false,
-      first: false,
-    }
-    if (React.Children.count(children) === index + 1) props.last = true
-    if (index === 0) props.first = true
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, props)
-    }
-    return child
-  })
-
-  return <Flex>{childrenWithProps}</Flex>
+  return <Flex>{children}</Flex>
 }
