121f58543afea1e893c23e887df7aa64b95d2e85
Adam Azad <adam@adamazad.com>
2022-09-20 19:06:20 +0100

    hotfix(analytics): fix queue

--- src/analytics/provider/analytics.provider.tsx
+++ src/analytics/provider/analytics.provider.tsx
@@ -1,7 +1,7 @@
 import { Trade } from '@swapr/sdk'
 
 import debugFactory from 'debug'
-import { useCallback, useEffect, useReducer, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { loadFathom } from '../fathom'
 import { siteEvents as siteEventsDev } from '../generated/dev'
@@ -9,7 +9,7 @@ import { FathomSiteInformation, siteEvents as siteEventsProd } from '../generate
 import * as trackers from '../trackers'
 import { computeTradeId } from '../utils'
 import { AnalyticsContext, IAnalyticsContext } from './analytics.context'
-import { ActionType, ItemStatus, reducer } from './analytics.state'
+import { AnalyticsTradeQueueState, ItemStatus } from './analytics.state'
 
 const debug = debugFactory('analytics')
 
@@ -21,46 +21,12 @@ export interface AnalyticsProviderProps {
  * AnalyticsProvider: provides the analytics context to the application
  */
 export function AnalyticsProvider({ children }: AnalyticsProviderProps) {
+  const timeoutRef = useRef<NodeJS.Timeout>()
   const [site, setSite] = useState<FathomSiteInformation>()
-  const [tradeQueue, tradeQueueDispatch] = useReducer(reducer, {})
+  const [tradeQueue, setTradeQueue] = useState<AnalyticsTradeQueueState>({})
 
-  /**
-   * Processes the trade queue
-   */
-  const processQueue = useCallback(async () => {
-    const pendingItems = Object.values(tradeQueue).filter(item => item.status === ItemStatus.PENDING)
-
-    for await (const pendingItem of pendingItems) {
-      trackers
-        .trackEcoEcoRouterTradeVolume(pendingItem.trade, site as FathomSiteInformation)
-        .then(() => {
-          tradeQueueDispatch({
-            type: ActionType.UPDATE,
-            payload: {
-              ...pendingItem,
-              status: ItemStatus.COMPLETE,
-            },
-          })
-        })
-        .catch(() => {
-          const retries = pendingItem.retries + 1
-          const status = retries >= 3 ? ItemStatus.FAILED : ItemStatus.PENDING
-
-          tradeQueueDispatch({
-            type: ActionType.UPDATE,
-            payload: {
-              ...pendingItem,
-              status,
-              retries,
-            },
-          })
-        })
-    }
-  }, [tradeQueue, site])
-
+  // Load the fathom site information
   useEffect(() => {
-    let processQueueTimeout: NodeJS.Timeout
-
     const siteId = process.env.REACT_APP_FATHOM_SITE_ID
     const siteScriptURL = process.env.REACT_APP_FATHOM_SITE_SCRIPT_URL
     const siteEvents = process.env.NODE_ENV === 'production' ? siteEventsProd : siteEventsDev
@@ -79,17 +45,10 @@ export function AnalyticsProvider({ children }: AnalyticsProviderProps) {
       return
     }
 
-    const startQueueProcessing = () => {
-      processQueueTimeout = setTimeout(() => {
-        processQueue().finally(startQueueProcessing)
-      }, 30000)
-    }
-
     loadFathom(siteId, siteScriptURL)
       .then(() => {
         debug('loadFathom: Fathom loaded. Setting siteEvents', { siteEvents })
         setSite(siteEvents as FathomSiteInformation)
-        startQueueProcessing() // Start the queue processing
       })
       .catch(error => {
         console.error('Error loading Fathom analytics', error)
@@ -98,11 +57,73 @@ export function AnalyticsProvider({ children }: AnalyticsProviderProps) {
     // clean up
     return () => {
       setSite(undefined)
-      tradeQueueDispatch({ type: ActionType.CLEAR })
-      clearTimeout(processQueueTimeout)
+      setTradeQueue({})
+      // tradeQueueDispatch({ type: ActionType.CLEAR })
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  // Queue processing
+  useEffect(() => {
+    if (!site) {
+      return
+    }
+
+    debug('useEffect: tradeQueue', { tradeQueue, 'timeoutRef.current': timeoutRef.current })
+
+    // clear the timeout
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+
+    /**
+     * Processes the trade queue
+     */
+    const processQueue = async () => {
+      debug('Processing queue', { site, tradeQueue })
+      const pendingItems = Object.values(tradeQueue).filter(item => item.status === ItemStatus.PENDING)
+
+      for await (const pendingItem of pendingItems) {
+        debug('Processing item', { pendingItem })
+        try {
+          await trackers.trackEcoEcoRouterTradeVolume(pendingItem.trade, site as FathomSiteInformation)
+
+          const payload = {
+            ...pendingItem,
+            status: ItemStatus.COMPLETE,
+          }
+
+          debug('Item processed', { pendingItem, payload })
+          setTradeQueue(state => ({ ...state, [pendingItem.id]: payload }))
+        } catch (error) {
+          const retries = pendingItem.retries + 1
+          const status = retries >= 5 ? ItemStatus.FAILED : ItemStatus.PENDING
+          const payload = {
+            ...pendingItem,
+            retries,
+            status,
+          }
+          debug('Error processing', { pendingItem, error, nextPayload: payload })
+          setTradeQueue(state => ({ ...state, [pendingItem.id]: payload }))
+        }
+      }
+
+      debug('Queue processed', { tradeQueue })
+    }
+
+    const startQueueProcessing = () => {
+      timeoutRef.current = setTimeout(() => {
+        processQueue().finally(startQueueProcessing)
+      }, 40000)
+    }
+
+    startQueueProcessing()
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+    }
+  }, [site, tradeQueue])
 
   const trackEcoEcoRouterTradeVolume = (trade: Trade) => {
     if (!site || !window.fathom) {
@@ -120,10 +141,10 @@ export function AnalyticsProvider({ children }: AnalyticsProviderProps) {
       .catch(error => {
         console.error('trackEcoEcoRouterTradeVolume: Error processing trade', { trade, error })
         const id = computeTradeId(trade)
-        tradeQueueDispatch({
-          type: ActionType.ADD,
-          payload: { id, trade, status: ItemStatus.PENDING, retries: 0 },
-        })
+        setTradeQueue(state => ({
+          ...state,
+          [id]: { id: computeTradeId(trade), trade, status: ItemStatus.PENDING, retries: 0 },
+        }))
       })
   }
 
