7bc32490286e5ca56e2d2bb3665f748ad2f2059d
Diogo <diogomf@hey.com>
2022-09-30 13:20:01 +0100

    Remove unused FindPoolTabs component

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -2,7 +2,6 @@ import { ArrowLeft } from 'react-feather'
 import styled from 'styled-components'
 
 import { useRouter } from '../../hooks/useRouter'
-import QuestionHelper from '../QuestionHelper'
 import { RowBetween } from '../Row'
 import { Settings } from '../Settings'
 
@@ -24,19 +23,6 @@ const StyledArrowLeft = styled(ArrowLeft)`
   color: ${({ theme }) => theme.purple3};
   cursor: pointer;
 `
-
-export function FindPoolTabs() {
-  const { navigate } = useRouter()
-  return (
-    <Tabs>
-      <RowBetween mb="16px">
-        <StyledArrowLeft onClick={() => navigate(-1)} />
-        <ActiveText>Import Pool</ActiveText>
-        <QuestionHelper text={"Use this tool to find pairs that don't automatically appear in the interface."} />
-      </RowBetween>
-    </Tabs>
-  )
-}
 
 export function AddRemoveTabs({ adding, creating }: { adding: boolean; creating: boolean }) {
   const { navigate } = useRouter()
