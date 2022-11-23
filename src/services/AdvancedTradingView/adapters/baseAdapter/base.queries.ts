9bfb980bbfdcf4ea2db7e17dd81ba9618cc75f1e
bejzik8 <bejzik8@gmail.com>
2022-11-23 02:17:18 +0100

    Created PAIR_SWAPS_BURNS_AND_MINTS query.

--- src/services/AdvancedTradingView/adapters/baseAdapter/base.queries.ts
+++ src/services/AdvancedTradingView/adapters/baseAdapter/base.queries.ts
@@ -2,6 +2,31 @@ import { gql } from 'graphql-request'
 
 import { BASE_QUERY_FRAGMENT } from '../fragment.queries'
 
+export const PAIR_SWAPS_BURNS_AND_MINTS = gql`
+  query getPairTradesAndActivity($pairId: Bytes!, $first: Int!, $skip: Int!) {
+    swaps(where: { pair: $pairId }, first: $first, skip: $skip, orderDirection: "desc", orderBy: "timestamp") {
+      id
+      transaction {
+        id
+      }
+      amount0In
+      amount1In
+      amount0Out
+      amount1Out
+      amountUSD
+      timestamp
+    }
+    mints(where: { pair: $pairId }, first: $first, skip: $skip, orderDirection: "desc", orderBy: "timestamp") {
+     ${BASE_QUERY_FRAGMENT}
+      type: __typename
+    }
+    burns(where: { pair: $pairId }, first: $first, skip: $skip, orderDirection: "desc", orderBy: "timestamp") {
+     ${BASE_QUERY_FRAGMENT}
+      type: __typename
+    }
+  }
+`
+
 export const PAIR_SWAPS = gql`
   query getPairTrades($pairId: Bytes!, $first: Int!, $skip: Int!) {
     swaps(where: { pair: $pairId }, first: $first, skip: $skip, orderDirection: "desc", orderBy: "timestamp") {
