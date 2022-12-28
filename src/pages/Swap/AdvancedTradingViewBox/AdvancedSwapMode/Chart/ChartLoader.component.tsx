49e6f620c0f3ba86bdea159041cb23c15f4a073b
Berteotti <berteotti2@gmail.com>
2022-12-28 10:58:16 +0000

    address comments

--- src/pages/Swap/AdvancedTradingViewBox/AdvancedSwapMode/Chart/ChartLoader.component.tsx
+++ src/pages/Swap/AdvancedTradingViewBox/AdvancedSwapMode/Chart/ChartLoader.component.tsx
@@ -14,7 +14,7 @@ export function ChartLoader({ pairAddress }: { pairAddress?: string }) {
     return () => clearTimeout(timeout)
   }, [pairAddress])
 
-  return isLoading ? (
+  return !pairAddress || isLoading ? (
     <AbsoluteWrapper>
       <FallbackLoader />
     </AbsoluteWrapper>
