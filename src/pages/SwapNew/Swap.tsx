d73e4607bce8b55e28bb662f0f769cab21d612f5
Mirko Basic <bejzik8@gmail.com>
2023-01-12 11:41:39 +0100

    Created CurrencyAmount component.

--- src/pages/SwapNew/Swap.tsx
+++ src/pages/SwapNew/Swap.tsx
@@ -10,7 +10,13 @@ export function Swapbox2() {
 }
 
 function CurrencyItem() {
-  return <CurrencyContainer></CurrencyContainer>
+  return (
+    <CurrencyContainer>
+      <ValueContainer>
+        <CurrencyAmount>1.488</CurrencyAmount>
+      </ValueContainer>
+    </CurrencyContainer>
+  )
 }
 
 const Container = styled.div`
@@ -23,9 +29,23 @@ const CurrencyContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 8px 8px 8px 22px;
   background: radial-gradient(173.28% 128.28% at 50.64% 0%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%),
     rgba(19, 19, 32, 0.5);
   border-radius: 12px;
   border: 1.5px solid #1b1b2a;
   margin-bottom: 6px;
+`
+
+const ValueContainer = styled.div``
+
+const CurrencyAmount = styled.p`
+  line-height: 34px;
+  font-size: 28px;
+  font-family: Inter;
+  font-weight: 600;
+  letter-spacing: 0.02em;
+  text-shadow: 0px 0px 12px rgba(255, 255, 255, 0.14);
+  color: #ffffff;
+  margin-bottom: 5px;
 `
