6cd092b8d9e4bbf825e61383c2aa49b7a6ab3450
Mirko Basic <bejzik8@gmail.com>
2023-01-12 11:14:38 +0100

    Added new swapbox route.

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -7,6 +7,7 @@ import { RedirectDuplicateTokenIds, RedirectOldAddLiquidityPathStructure } from 
 import { RedirectOldRemoveLiquidityPathStructure } from './Pools/RemoveLiquidity/redirects'
 // Dont want to lazy import swap
 import { Swap } from './Swap'
+import { Swapbox2 } from './SwapNew/Swap'
 
 // Lazy loaded routes
 const Rewards = lazy(() => import(/* webpackPrefetch: true */ './Rewards'))
@@ -23,6 +24,7 @@ const Account = lazy(() => import(/* webpackPrefetch: true */ './Account'))
 export function Routes() {
   return (
     <Switch>
+      <Route path="swapbox2" element={<Swapbox2 />} />
       <Route path="swap" element={<Swap />} />
       <Route path="swap/pro" element={<Swap />} />
       <Route path="swap/:outputCurrency" element={<BaseRedirect />} />
