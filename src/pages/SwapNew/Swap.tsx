ab92b36534e015ac2511cb273b4f71dcf69fdba8
Mirko Basic <bejzik8@gmail.com>
2023-01-12 11:37:03 +0100

    Working on CurrencyItem component.

--- src/pages/SwapNew/Swap.tsx
+++ src/pages/SwapNew/Swap.tsx
@@ -9,15 +9,23 @@ export function Swapbox2() {
   )
 }
 
+function CurrencyItem() {
+  return <CurrencyContainer></CurrencyContainer>
+}
+
 const Container = styled.div`
   width: 467px;
 `
 
-const CurrencyItem = styled.div`
+const CurrencyContainer = styled.div`
   width: 100%;
   height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background: radial-gradient(173.28% 128.28% at 50.64% 0%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%),
     rgba(19, 19, 32, 0.5);
   border-radius: 12px;
   border: 1.5px solid #1b1b2a;
+  margin-bottom: 6px;
 `
