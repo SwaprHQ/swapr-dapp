08ac1edd009965dcc2b5e72d1138ab92dbb0f1f7
Mirko Basic <bejzik8@gmail.com>
2023-01-14 16:21:16 +0100

    Created SwapButton component.

---
+++ src/pages/SwapNew/components/SwapButton.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+import { ReactComponent as CowSVG } from '../../../assets/swapbox/dex-logo-cow.svg'
+import { SharedStyles } from './styles'
+
+export function SwapButton() {
+  return (
+    <StyledButton>
+      <SwapButtonLabel>Swap With</SwapButtonLabel>
+      <CowSVG />
+      <SwapButtonLabel>Cow</SwapButtonLabel>
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border: none;
+  border-radius: 12px;
+  background: linear-gradient(93.39deg, #2b00a4 -8.9%, #d67b5a 114.08%);
+  box-shadow: 0px 0px 42px rgba(129, 62, 127, 0.32);
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`
+
+const SwapButtonLabel = styled.p`
+  display: inline-block;
+  line-height: 16px;
+  font-size: 13px;
+  ${SharedStyles}
+  font-weight: 600;
+  letter-spacing: 0.08em;
+  text-transform: uppercase;
+  color: #ffffff;
+`
