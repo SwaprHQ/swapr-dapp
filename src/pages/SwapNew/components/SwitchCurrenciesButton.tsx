75ffd9f7086828fee7daf9d54e8ba9bd6d986e2b
Mirko Basic <bejzik8@gmail.com>
2023-01-14 17:06:52 +0100

    Created SwitchCurrenciesButton component.

---
+++ src/pages/SwapNew/components/SwitchCurrenciesButton.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+import { ReactComponent as SwapArrowSVG } from '../../../assets/swapbox/swap-arrow.svg'
+
+export function SwitchCurrenciesButton() {
+  return (
+    <StyledButton>
+      <SwapArrowSVG />
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button`
+  width: 36px;
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 50%;
+  top: 84px;
+  transform: translateX(-50%);
+  background: #06060a;
+  border-radius: 12px;
+  border: 1px solid #0c0c14;
+  box-shadow: 0px 0px 42px rgba(0, 0, 0, 0.42);
+  backdrop-filter: blur(11px);
+`
