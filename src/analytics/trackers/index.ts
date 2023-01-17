3b86dfc3a5757d0b2bae6a34e83e753bf7303b4c
Adam Azad <adam@adamazad.com>
2023-01-17 16:24:58 +0000

    feat: add fathom analytics to ecobridge (#1682)
    * feat(#1667): add ecoBridge event generator
    
    * feat(#1667): update fathom events with EcoBridge
    
    * feat(#1667): add EcoBridge trackers

--- src/analytics/trackers/index.ts
+++ src/analytics/trackers/index.ts
@@ -1,9 +1,16 @@
+import { AddressZero } from '@ethersproject/constants'
 import { Trade } from '@swapr/sdk'
 
 import debugFactory from 'debug'
 
-import { getTradeUSDValue } from '../../utils/coingecko'
-import { FathomSiteInformation, getEcoRouterVolumeUSDEventName, getNetworkNameByChainId } from '../generated/prod'
+import { BridgeTransactionSummary } from '../../state/bridgeTransactions/types'
+import { getTradeUSDValue, getUSDPriceCurrencyQuote, getUSDPriceTokenQuote } from '../../utils/coingecko'
+import {
+  FathomSiteInformation,
+  getEcoBridgeVolumeUSDEventName,
+  getEcoRouterVolumeUSDEventName,
+  getNetworkNameByChainId,
+} from '../generated/prod'
 
 const debug = debugFactory('analytics:trackers')
 
@@ -13,8 +20,8 @@ const debug = debugFactory('analytics:trackers')
  * @returns void
  * @throws Error on null USD value and when
  */
-export async function trackEcoEcoRouterTradeVolume(trade: Trade, site: FathomSiteInformation) {
-  debug('tracking trade USD volume', { trade, site })
+export async function trackEcoRouterTradeVolume(trade: Trade, site: FathomSiteInformation) {
+  debug('tracking EcoRouter trade USD volume', { trade, site })
   // Get use value for input amount
   const tradeUSDValue = await getTradeUSDValue(trade)
 
@@ -30,7 +37,44 @@ export async function trackEcoEcoRouterTradeVolume(trade: Trade, site: FathomSit
   const eventId = site.events.find(event => event.name === eventName)?.id
 
   if (!eventId) {
-    throw new Error(`Event ID for (${eventName}) not found`)
+    throw new Error(`Event ID for (${eventName}) not found in site (${site.siteId})`)
+  }
+
+  window.fathom.trackGoal(eventId, tradeUSDValueInCents)
+}
+
+/**
+ * Processes the trade volume event
+ * @param trade Trade
+ * @returns void
+ * @throws Error on null USD value and when
+ */
+export async function trackEcoBridgeTradeVolume(trade: BridgeTransactionSummary, site: FathomSiteInformation) {
+  debug('tracking EcoBridge trade USD volume', { trade, site })
+
+  const isNativeToken = (trade.assetAddressL1 === AddressZero || trade.assetAddressL1 === 'XDAI') && trade.fromChainId
+
+  let rawTokenPriceInfo = isNativeToken
+    ? await getUSDPriceCurrencyQuote({
+        chainId: trade.fromChainId,
+      })
+    : await getUSDPriceTokenQuote({
+        tokenAddress: trade.assetAddressL1,
+        chainId: trade.fromChainId,
+      })
+
+  if (rawTokenPriceInfo === null) {
+    throw new Error('Could not get token price')
+  }
+
+  const tokenUSDPrice = Object.values(rawTokenPriceInfo)[0].usd
+  const usdValue = (tokenUSDPrice * parseFloat(trade.fromValue)).toFixed(2)
+  const tradeUSDValueInCents = (parseFloat(parseFloat(usdValue).toFixed(2)) * 100).toString() // convert to cents because fathom requires it
+  const eventName = getEcoBridgeVolumeUSDEventName(trade.bridgeId, trade.fromChainId, trade.toChainId)
+  const eventId = site.events.find(event => event.name === eventName)?.id
+
+  if (!eventId) {
+    throw new Error(`Event ID for (${eventName}) not found in site (${site.siteId})`)
   }
 
   window.fathom.trackGoal(eventId, tradeUSDValueInCents)
