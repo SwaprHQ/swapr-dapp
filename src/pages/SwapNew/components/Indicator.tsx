68fde9eff8bfb1c8ac795393bcc61e52484df9a4
Mirko Basic <bejzik8@gmail.com>
2023-01-18 23:35:05 +0100

    Updated Indicator component.

--- src/pages/SwapNew/components/Indicator.tsx
+++ src/pages/SwapNew/components/Indicator.tsx
@@ -4,66 +4,32 @@ import { ReactComponent as BananaSVG } from '../../../assets/swapbox/banana.svg'
 import { ReactComponent as DexesSVG } from '../../../assets/swapbox/dexes.svg'
 import { ReactComponent as GasSVG } from '../../../assets/swapbox/gas.svg'
 import { ReactComponent as ShieldSVG } from '../../../assets/swapbox/shield.svg'
-import {
-  INDICATOR_COLOR_NEGATIVE,
-  INDICATOR_COLOR_POSITIVE,
-  INDICATOR_COLOR_UNDEFINED,
-  INDICATOR_COLOR_WARNING,
-} from '../constants'
+import { INDICATOR_COLOR, IndicatorColorVariant, IndicatorIconVariant } from '../constants'
 
-export enum IndicatorVariant {
-  POSITIVE = 'POSITIVE',
-  WARNING = 'WARNING',
-  NEGATIVE = 'NEGATIVE',
-  UNDEFINED = 'UNDEFINED',
+export const INDICATOR_ICON = {
+  [IndicatorIconVariant.DEXES]: <DexesSVG />,
+  [IndicatorIconVariant.GAS]: <GasSVG />,
+  [IndicatorIconVariant.BANANA]: <BananaSVG />,
+  [IndicatorIconVariant.SHIELD]: <ShieldSVG />,
 }
 
 type IndicatorProps = {
-  variant: IndicatorVariant
-  icon: 'dexes' | 'gas' | 'banana' | 'shield'
+  color: IndicatorColorVariant
+  icon: IndicatorIconVariant
 }
 
-export function Indicator({ variant, icon }: IndicatorProps) {
-  return (
-    <Container variant={variant}>
-      {(() => {
-        switch (icon) {
-          case 'dexes':
-            return <DexesSVG />
-          case 'gas':
-            return <GasSVG />
-          case 'banana':
-            return <BananaSVG />
-          case 'shield':
-            return <ShieldSVG />
-          default:
-            return null
-        }
-      })()}
-    </Container>
-  )
+export function Indicator({ color, icon }: IndicatorProps) {
+  return <Container color={color}>{INDICATOR_ICON[icon]}</Container>
 }
 
-const Container = styled.div<{ variant: IndicatorVariant }>`
+const Container = styled.div<{ color: IndicatorColorVariant }>`
   height: 20px;
   display: inline-flex;
   align-items: center;
   padding: 5px;
   background: rgba(14, 159, 110, 0.08);
   border-radius: 4px;
-  border: 1px solid
-    ${({ variant }) => {
-      switch (variant) {
-        case IndicatorVariant.POSITIVE:
-          return INDICATOR_COLOR_POSITIVE
-        case IndicatorVariant.WARNING:
-          return INDICATOR_COLOR_WARNING
-        case IndicatorVariant.NEGATIVE:
-          return INDICATOR_COLOR_NEGATIVE
-        case IndicatorVariant.UNDEFINED:
-          return INDICATOR_COLOR_UNDEFINED
-      }
-    }};
+  border: 1px solid ${({ color }) => INDICATOR_COLOR[color]};
   box-shadow: 0px 0px 8px rgba(16, 158, 110, 0.15);
   margin-right: 4px;
 `
