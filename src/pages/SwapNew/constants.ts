7a958dffa7194c4d55f72bce1c08300fa074d441
Mirko Basic <bejzik8@gmail.com>
2023-01-18 16:57:52 +0100

    Created constants file.

---
+++ src/pages/SwapNew/constants.ts
@@ -0,0 +1,4 @@
+export const SWAPBOX_WIDTH = 'min(467px, 100%)'
+export const SWAPBOX_ELEMENTS_BACKGROUND =
+  'radial-gradient(173.28% 128.28% at 50.64% 0%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%), rgba(19, 19, 32, 0.5)'
+export const SWAPBOX_ELEMENTS_SPACING = '6px'
