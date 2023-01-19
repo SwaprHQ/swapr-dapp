d606095d5188e75b6cc9634f25c2d72ee75ad0bf
Mirko Basic <bejzik8@gmail.com>
2023-01-19 19:22:03 +0100

    Updated styling of CurrencyItem component.

--- src/pages/SwapNew/components/CurrencyItem.tsx
+++ src/pages/SwapNew/components/CurrencyItem.tsx
@@ -6,6 +6,8 @@ import { ReactComponent as EtherSVG } from '../../../assets/swapbox/ether.svg'
 import {
   ELEMENTS_BACKGROUND_PRIMARY,
   ELEMENTS_BACKGROUND_SECONDARY,
+  ELEMENTS_BORDER_PRIMARY,
+  ELEMENTS_BORDER_SECONDARY,
   ELEMENTS_SPACING,
   TEXT_COLOR_PRIMARY,
   TEXT_COLOR_SECONDARY,
@@ -46,6 +48,7 @@ export function CurrencyItem({ lowerItem }: CurrencyItemProps) {
 const CurrencyContainer = styled.div<{ lowerItem?: boolean }>`
   width: 100%;
   height: 100px;
+  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -53,6 +56,22 @@ const CurrencyContainer = styled.div<{ lowerItem?: boolean }>`
   background: ${({ lowerItem }) => (lowerItem ? ELEMENTS_BACKGROUND_SECONDARY : ELEMENTS_BACKGROUND_PRIMARY)};
   ${BorderStyle}
   margin-bottom: ${ELEMENTS_SPACING};
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 12px;
+    border: 1.5px solid transparent;
+    background: ${({ lowerItem }) => (lowerItem ? ELEMENTS_BORDER_SECONDARY : ELEMENTS_BORDER_PRIMARY)};
+    mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
+    -webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+  }
 `
 
 const ValueContainer = styled.div`
