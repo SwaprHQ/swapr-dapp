2623ce86a03e7c3e8f30f3c85450667f8c068746
Mirko Basic <bejzik8@gmail.com>
2023-01-19 19:21:31 +0100

    Added new constants.

--- src/pages/SwapNew/constants.ts
+++ src/pages/SwapNew/constants.ts
@@ -3,6 +3,10 @@ export const ELEMENTS_BACKGROUND_PRIMARY =
   'radial-gradient(173.28% 128.28% at 50.64% 0%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%), rgba(19, 19, 32, 0.5)'
 export const ELEMENTS_BACKGROUND_SECONDARY =
   'radial-gradient(160.32% 118.69% at 50.64% 100%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%), rgba(19, 19, 32, 0.5)'
+export const ELEMENTS_BORDER_PRIMARY =
+  'linear-gradient(0deg, rgba(68, 65, 99, 0.05), rgba(68, 65, 99, 0.05)) border-box, radial-gradient(40.58% 91.41% at 50.11% -12.63%, rgba(167, 162, 223, 0.1) 0%, rgba(37, 35, 57, 0.02) 100%) border-box'
+export const ELEMENTS_BORDER_SECONDARY =
+  'linear-gradient(0deg, rgba(68, 65, 99, 0.05), rgba(68, 65, 99, 0.05)) border-box, radial-gradient(30.27% 68.18% at 50.11% 100%, rgba(167, 162, 223, 0.1) 0%, rgba(37, 35, 57, 0.02) 100%) border-box'
 export const ELEMENTS_SPACING = '6px'
 export const TEXT_COLOR_PRIMARY = '#fff'
 export const TEXT_COLOR_SECONDARY = '#8780bf'
