84d3bb95cd876212ab7bd2c74854e90da4245d08
Mirko Basic <bejzik8@gmail.com>
2023-01-26 15:12:53 +0100

    Created Input component.

--- src/pages/SwapNew/components/TokenPicker.tsx
+++ src/pages/SwapNew/components/TokenPicker.tsx
@@ -14,7 +14,12 @@ export function TokenPicker() {
     }
   }, [tokenPickerContainer])
 
-  return createPortal(<Container></Container>, tokenPickerContainer)
+  return createPortal(
+    <Container>
+      <Input placeholder="Search token by name or paste address" spellCheck={false} />
+    </Container>,
+    tokenPickerContainer
+  )
 }
 
 const Container = styled.div`
@@ -24,6 +29,27 @@ const Container = styled.div`
   bottom: 0;
   left: 0;
   z-index: 1;
+  display: flex;
+  justify-content: center;
   background: rgba(255, 255, 255, 0.01);
   backdrop-filter: blur(7px);
+`
+
+const Input = styled.input`
+  width: 478px;
+  height: 48px;
+  box-sizing: border-box;
+  background: rgba(20, 18, 31, 0.5);
+  border-radius: 12px;
+  border: 2px solid #8c83c0;
+  outline: none;
+  line-height: 14px;
+  font-size: 16px;
+  font-family: Inter;
+  font-weight: 500;
+  color: #dddaf8;
+  text-shadow: 0px 0px 10px rgba(255, 255, 255, 0.15);
+  padding: 15px 20px;
+  backdrop-filter: blur(12.5px);
+  margin-top: 220px;
 `
