dbe6aaca05c8faaf691fd45d8f47f25f53d0b7d4
bejzik8 <bejzik8@gmail.com>
2023-02-02 21:02:51 +0100

    Updated renderCurrencyLogo function.

--- src/pages/SwapNew/utils/index.tsx
+++ src/pages/SwapNew/utils/index.tsx
@@ -1,7 +1,7 @@
 import { currencies, CurrencySymbol } from '../constants'
 
 export function renderCurrencyLogo(currencySymbol: CurrencySymbol) {
-  const Logo = currencies[currencySymbol]
+  const Logo = currencies[currencySymbol].logo
 
   return <Logo />
 }
