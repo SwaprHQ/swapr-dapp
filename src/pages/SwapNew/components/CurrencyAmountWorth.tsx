0a2c18b51926bda0f30a8571e8b1f97772f8ab49
bejzik8 <bejzik8@gmail.com>
2023-02-22 13:19:37 +0100

    Created CurrencyAmountWorth component.

---
+++ src/pages/SwapNew/components/CurrencyAmountWorth.tsx
@@ -0,0 +1,29 @@
+import { CurrencyAmount, JSBI, Percent } from '@swapr/sdk'
+
+import styled from 'styled-components'
+
+import { TEXT_COLOR_SECONDARY } from '../constants'
+import { FontFamily } from './styles'
+
+type CurrencyAmountWorthProps = {
+  fiatValue?: CurrencyAmount | null
+  priceImpact?: Percent
+  isFallback?: boolean
+}
+
+export function CurrencyAmountWorth({ fiatValue, priceImpact, isFallback }: CurrencyAmountWorthProps) {
+  console.log('VALUES', fiatValue, priceImpact, isFallback)
+
+  const value = fiatValue ? `${isFallback && '~'}$${fiatValue.toFixed(2, { groupSeparator: ',' })}` : ''
+
+  return <Paragraph>{value}</Paragraph>
+}
+
+const Paragraph = styled.p`
+  line-height: 12px;
+  font-size: 10px;
+  ${FontFamily}
+  font-weight: 600;
+  letter-spacing: 0.08em;
+  color: ${TEXT_COLOR_SECONDARY};
+`
