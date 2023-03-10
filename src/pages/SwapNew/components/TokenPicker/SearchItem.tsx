b814c790daf5bca05ae27f1c694d8cd27975eb3e
bejzik8 <bejzik8@gmail.com>
2023-03-10 14:13:10 -0800

    Created SearchItem component.

---
+++ src/pages/SwapNew/components/TokenPicker/SearchItem.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+type SearchItemProps = {}
+
+export function SearchItem({}: SearchItemProps) {
+  return <Container>Currency</Container>
+}
+
+const Container = styled.div`
+  margin-bottom: 16px;
+`
