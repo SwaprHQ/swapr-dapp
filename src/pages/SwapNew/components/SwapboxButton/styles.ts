c8fca2f1973494a8afdc2fa1a3ea17d2f5bae54b
bejzik8 <bejzik8@gmail.com>
2023-03-29 14:22:12 +0200

    Added a few platform colors.

--- src/pages/SwapNew/components/SwapboxButton/styles.ts
+++ src/pages/SwapNew/components/SwapboxButton/styles.ts
@@ -11,6 +11,8 @@ import { FontFamily } from '../styles'
 const COW_SWAP_COLOR = 'linear-gradient(93.39deg, #2b00a4 -8.9%, #d67b5a 114.08%)'
 const SUSHI_SWAP_COLOR = 'linear-gradient(93.39deg, #2B00A4 -8.9%, #CD45B4 114.08%)'
 const ONE_INCH_COLOR = 'linear-gradient(93.39deg, #D82122 -8.9%, #1B314F 114.08%)'
+const CURVE_COLOR = 'linear-gradient(93.39deg, #24FFD3 -8.9%, #D10000 114.08%)'
+const UNISWAP_COLOR = 'linear-gradient(93.39deg, #FF007A -8.9%, #0C192B 114.08%)'
 
 const getBackgroundColor = (platformName?: string) => {
   console.log('PLATFORM', platformName)
@@ -23,6 +25,12 @@ const getBackgroundColor = (platformName?: string) => {
       return SUSHI_SWAP_COLOR
     case '1Inch':
       return ONE_INCH_COLOR
+    case 'Curve':
+      return CURVE_COLOR
+    case 'Uniswap':
+      return UNISWAP_COLOR
+    case 'Uniswap v2':
+      return UNISWAP_COLOR
     default:
       return 'rgb(46, 23, 242)'
   }
