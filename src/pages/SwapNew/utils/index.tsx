b5a804bfd994ebbe4e810063134538d898d1f1ac
bejzik8 <bejzik8@gmail.com>
2023-03-31 13:14:43 +0200

    Removed unused files.

--- src/pages/SwapNew/utils/index.tsx
+++
@@ -1,7 +0,0 @@
-import { currencies, CurrencySymbol } from '../constants'
-
-export function renderCurrencyLogo(currencySymbol: CurrencySymbol) {
-  const Logo = currencies[currencySymbol].logo
-
-  return <Logo />
-}
