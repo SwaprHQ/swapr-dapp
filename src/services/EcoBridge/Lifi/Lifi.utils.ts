903cd43d17bef2e093735f1655e329154075a394
Wixzi <wixzi@pm.me>
2023-04-02 16:16:42 +0200

    feat: Move getStatus to utils

--- src/services/EcoBridge/Lifi/Lifi.utils.ts
+++ src/services/EcoBridge/Lifi/Lifi.utils.ts
@@ -1,6 +1,9 @@
 import { ChainId as SwaprChainId } from '@swapr/sdk'
 
-import { ChainId } from '@lifi/sdk'
+import { ChainId, StatusResponse } from '@lifi/sdk'
+
+import { BridgeTransactionStatus } from '../../../state/bridgeTransactions/types'
+import { LIFI_TXN_STATUS } from './Lifi.constants'
 
 // // Add more chains keys on the go from
 // //'https://li.quest/v1/chains';
@@ -16,4 +19,16 @@ export const LifiChainShortNames = new Map([
 
 export function isLifiChainId(value: any): value is ChainId {
   return Object.values(ChainId).indexOf(value) > -1
+}
+
+export function getStatus(status: StatusResponse['status']) {
+  switch (status) {
+    case LIFI_TXN_STATUS.DONE:
+      return BridgeTransactionStatus.CONFIRMED
+    case LIFI_TXN_STATUS.FAILED:
+    case LIFI_TXN_STATUS.INVALID:
+      return BridgeTransactionStatus.FAILED
+    default:
+      return BridgeTransactionStatus.PENDING
+  }
 }
