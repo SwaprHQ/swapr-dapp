b0600a637abc5678647018c317698917387651c9
bejzik8 <bejzik8@gmail.com>
2023-04-04 14:49:11 +0200

    Updated Notification styling.

--- src/pages/SwapNew/components/Notification/index.tsx
+++ src/pages/SwapNew/components/Notification/index.tsx
@@ -1,3 +1,4 @@
+import { AlertTriangle } from 'react-feather'
 import styled from 'styled-components'
 
 import { BorderStyle, ELEMENTS_SPACING } from '../../constants'
@@ -5,16 +6,34 @@ import { BorderStyle, ELEMENTS_SPACING } from '../../constants'
 export function Notification() {
   return (
     <Container>
+      <SwapCallbackErrorInnerAlertTriangle>
+        <AlertTriangle size={18} />
+      </SwapCallbackErrorInnerAlertTriangle>
       <Text>This is a notification!</Text>
     </Container>
   )
 }
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 8px 22px;
   ${BorderStyle}
   background-color: rgba(240, 46, 81, 0.1);
   margin-top: ${ELEMENTS_SPACING};
+`
+
+const SwapCallbackErrorInnerAlertTriangle = styled.div`
+  height: 32px;
+  width: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  color: red;
+  background-color: rgba(240, 46, 81, 0.1);
+  margin-right: 8px;
 `
 
 const Text = styled.p`
@@ -22,4 +41,7 @@ const Text = styled.p`
   font-size: 12px;
   color: #f02e51;
   text-transform: uppercase;
+  user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
 `
