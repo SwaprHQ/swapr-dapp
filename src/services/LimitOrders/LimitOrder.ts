fd07d82ca6fbe7f45c7e5b5984e9c6180c79736e
Wixzi <wixzi@pm.me>
2023-06-15 10:11:06 +0200

    [SWA-46] Create Limit Order class

---
+++ src/services/LimitOrders/LimitOrder.ts
@@ -0,0 +1,31 @@
+import { limitOrderConfig } from './LimitOrder.config'
+import { LimitOrderChangeHandler } from './LimitOrder.types'
+import { LimitOrderBase } from './LimitOrder.utils'
+
+export class LimitOrder {
+  #protocols: LimitOrderBase[]
+  activeProtocol?: LimitOrderBase
+
+  constructor() {
+    console.log('LimitOrder constructor')
+    this.#protocols = limitOrderConfig
+  }
+
+  updateSigner = async (signerData: LimitOrderChangeHandler) => {
+    console.log('LimitOrder updateSigner')
+    await Promise.all(
+      Object.values(this.#protocols).map(async protocol => {
+        await protocol.setSignerData(signerData)
+        await protocol.onSignerChange(signerData)
+      })
+    )
+  }
+
+  getactiveProtocol = async () => {
+    console.log('LimitOrder getactiveProtocols')
+    this.activeProtocol = this.#protocols.find(
+      protocol => protocol.activeChainId && protocol.supportedChanins.includes(protocol.activeChainId)
+    )
+    return this.activeProtocol
+  }
+}
