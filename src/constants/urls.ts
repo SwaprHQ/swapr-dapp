f3e59bd74f1e9ee28cbfeeb6e02ccc0562383060
Rorry <rodrigojali@gmail.com>
2023-12-21 17:44:29 -0300

    chore(SWA-128): PR comment to consume env vars with fallbacks for our URLs constants

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const DISCORD_INVITE_LINK = 'https://discord.com/invite/QFkNsjTkzD'
-export const LIQUIDITY_V3_LINK = 'https://swapr.liquidity.eth.limo/#/info/pools'
+export const DISCORD_INVITE_LINK = process.env.DISCORD_INVITE_URL ?? 'https://discord.com/invite/QFkNsjTkzD'
+export const LIQUIDITY_V3_LINK = process.env.LIQUIDITY_V3_URL ?? 'https://swapr.liquidity.eth.limo/#/info/pools'
