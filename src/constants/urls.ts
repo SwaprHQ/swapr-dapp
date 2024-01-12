ccd98415def280fbf9f935a2c679fea0a620d05d
Berteotti <berteotti2@gmail.com>
2024-01-12 10:57:22 +0000

    chore: update liquidity V3 link

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,3 @@
 export const DISCORD_INVITE_LINK = process.env.DISCORD_INVITE_URL ?? 'https://discord.com/invite/QFkNsjTkzD'
-export const LIQUIDITY_V3_LINK = process.env.LIQUIDITY_V3_URL ?? 'https://swapr.liquidity.eth.limo/#/info/pools'
+export const LIQUIDITY_V3_LINK = process.env.LIQUIDITY_V3_URL ?? 'https://v3.swapr.eth.limo/'
+export const LIQUIDITY_V3_INFO_POOLS_LINK = `${LIQUIDITY_V3_LINK}#/info/pools`
