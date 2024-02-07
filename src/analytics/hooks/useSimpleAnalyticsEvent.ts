5499b63561052b6e5dfb22f02962a1fbc91efe26
Rorry <lil.rorry@gmail.com>
2024-02-07 10:37:34 -0300

    improv(SWA-144): update the FA tracking implementation due to the new API (#1908)

--- src/analytics/hooks/useSimpleAnalyticsEvent.ts
+++ src/analytics/hooks/useSimpleAnalyticsEvent.ts
@@ -1,20 +1,14 @@
 import { useAnalytics } from './analytics.hooks'
 
 export const useSimpleAnalyticsEvent = () => {
-  const { site, fathom } = useAnalytics()
+  const { site } = useAnalytics()
 
-  const trackEvent = (eventName: string) => {
+  const trackEvent = (eventName: string, opt?: { _site_id: string; _value: number }) => {
     if (!site || !window.fathom) {
       return console.error('Fathom site not found', { site, fathom: window.fathom })
     }
 
-    const eventId = site.events.find(({ name }) => name === eventName)?.id
-
-    if (!eventId) {
-      return console.error(`Event ID for (${eventName}) not found in site (${site.siteId})`)
-    }
-
-    fathom.trackGoal(eventId, 0)
+    window.fathom.trackEvent(eventName, opt)
   }
 
   return trackEvent
