8981078a9b233a539f0c571f6197dbc3a343985a
Diogo Ferreira <diogomf@hey.com>
2024-03-01 17:39:36 +0000

    small updates and clean up - update logo - removed badge from logo - remove "pro" tab - remove bridge tab

--- src/components/SwaprVersionLogo/index.tsx
+++ src/components/SwaprVersionLogo/index.tsx
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import packageJson from '../../../package.json'
 import logoImage from '../../assets/images/swapr_white_no_badge.svg'
 import { breakpoints } from '../../utils/theme'
 
@@ -12,29 +12,10 @@ const Logo = styled.img.attrs({ src: logoImage })`
   }
 `
 
-const RelativeContainer = styled.div`
-  position: relative;
-`
-
-const Badge = styled.div`
-  position: absolute;
-  bottom: -6px;
-  right: 0;
-  background: ${({ theme }) => theme.bg3};
-  padding: 3px 4px;
-  color: ${({ theme }) => theme.text1};
-  border-radius: 4px;
-  font-size: 8px;
-  font-weight: 700;
-  letter-spacing: 0.16em;
-  text-transform: uppercase;
-`
-
 export function SwaprVersionLogo() {
   return (
-    <RelativeContainer>
+    <Link to="/">
       <Logo />
-      <Badge>{packageJson.version}</Badge>
-    </RelativeContainer>
+    </Link>
   )
 }
