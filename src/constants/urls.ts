8d7db44d1f44528a84ee8a2cbda01c0916da01bd
Diogo Ferreira <diogomf@hey.com>
2024-03-06 18:18:26 +0000

    update mobile links

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,4 @@
 export const DISCORD_INVITE_LINK = process.env.DISCORD_INVITE_URL ?? 'https://discord.com/invite/QFkNsjTkzD'
 export const LIQUIDITY_V3_LINK = process.env.LIQUIDITY_V3_URL ?? 'https://v3.swapr.eth.limo/'
 export const LIQUIDITY_V3_INFO_POOLS_LINK = `${LIQUIDITY_V3_LINK}#/info/pools`
+export const STACKLY_URL = process.env.REACT_APP_STACKLY_URL ?? 'https://www.stackly.app/'
