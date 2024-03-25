8a1b5732493aee2fe1afdf7fa2ca465b26c39fc7
Rorry <lil.rorry@gmail.com>
2024-03-25 10:32:06 -0300

    docs(SWA-138): update docs url (#1917)
    * feat(SWA-146): add Openocean support (#1913)
    * docs(SWA-138): update footer links to docs URLs
    
    ---------
    
    Co-authored-by: Berteotti <berteotti2@gmail.com>

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,3 +2,8 @@ export const DISCORD_INVITE_LINK = process.env.DISCORD_INVITE_URL ?? 'https://di
 export const LIQUIDITY_V3_LINK = process.env.LIQUIDITY_V3_URL ?? 'https://v3.swapr.eth.limo/'
 export const LIQUIDITY_V3_INFO_POOLS_LINK = `${LIQUIDITY_V3_LINK}#/info/pools`
 export const STACKLY_URL = process.env.REACT_APP_STACKLY_URL ?? 'https://www.stackly.app/'
+export const SWAPR_DOCS_BASE_URL = 'https://swapr-eth.gitbook.io/swapr'
+export const SWAPR_DOCS_PATH_URLS = {
+  FAQ: '/faq',
+  PROJECT_INFO_BRANDING: '/project-information/branding-and-logos',
+}
